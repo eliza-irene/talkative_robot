0f61121f10b15b68f654b73e0a7aef8c8f32c065
eliza-irene <elizabeth.korthof@gmail.com>
2014-09-24 09:44:28 -0400

    Finished Lab 2 exercises

--- loop_test.rb
+++ loop_test.rb
@@ -1,13 +1,17 @@
-puts "Do you have a pet?"
+pet = nil
+
+loop do 
+puts "Do you have a pet? (yes or no)"
 pet = gets.chomp.downcase
 
-while not pet == "yes" && pet == "no"
+if pet == "yes" || pet == "no"
+
 
 	if pet == "yes"
 		puts "Great! Your spirit animal is proud!"
-	elsif pet == "no" 
+	else pet == "no" 
 		puts "I'm sorry. :( Your spirit animal is disappointed."
-	else
-		puts "You didn't answer with 'yes' or 'no'"
 	end
+		break
+end
 end
--- talkative_robot.rb
+++ talkative_robot.rb
@@ -1,37 +1,42 @@
 require 'pry'
 
-puts "Howdy!!"
-
+puts "Let's get started.  I'll need your name, age, and gender."
 puts "What's your name?"
 name = gets.chomp.capitalize
 
 puts "How old are you?"
 age = gets.chomp.to_i
+puts "You can drive!" if age >= 16
 
-puts "Hi #{name} who is #{age} years old!"
+puts "Are you male or female?"
+gender = gets.chomp.downcase
+
+gender_greeting = gender == "female" ? "Ms." : "Mr."
+age_message = age >= 21 ? "You're old enough to drink! Yes!" : "You're not old enough to drink."
+puts "Hi #{gender_greeting} #{name} who is #{age} years old. #{age_message}"
 
 first_initial = name.chars.first
-
 puts "Do you mind if I call you #{first_initial}?"
 initial_answer = gets.chomp.downcase
-	if initial_answer == "no"
-		puts "Well that's strange! I'm going to call you by your full name anyway."
-	elsif initial_answer == "yes"
-		puts "Don't worry, I don't like it either."
-	else 
-		puts "You didn't answer with yes or no."
-	end
-		
-future_age = 75 - age
-puts "You will be 75 years old in #{future_age} years."
+puts initial_answer == "no" ? "Okay." : "Don't worry about it."
+			
+case
+when age < 75
+	young_future_age = 75 - age
+	puts "You will be 75 years old in #{young_future_age} years!"
+when age > 75
+	old_future_age = age - 75
+	puts "#{old_future_age} years ago you turned 75! Wow, you're up there!"
+when age = 75
+	puts "You're exactly 75 years old!"
+end
 
 name.upcase!
 puts "Hey #{name}, where are you going!?"
 
-puts "Yo 'Dude', what's up?"
+puts "Yo 'Dude', are you having a good day?"
 whats_up = gets.chomp
-
-puts "Really?  Your answer is #{whats_up}?!"
+puts "That's good news." unless whats_up == "no"
 
 puts "What's your favorite color?"
 color = gets.chomp.upcase
@@ -39,15 +44,16 @@ color = gets.chomp.upcase
 puts "What is your spirit animal?"
 spirit_animal = gets.chomp.upcase
 
-puts "#{name}, you have now become the mighty #{spirit_animal} whose power color is #{color}!  You are almost ready to take over the world!!!"
+puts "#{name}, you are now channeling the mighty #{spirit_animal} whose power color is #{color}!  You are almost ready to take over the world!!!"
 
-puts "Do you have a pet?"
+loop do 
+puts "Do you have a pet? (yes or no)"
 pet = gets.chomp.downcase
+if pet == "yes" || pet == "no"
+puts pet == "yes" ? "Great! Your spirit animal is proud!" : "I'm sorry. :( Your spirit animal is disappointed."
+	break
+end
+end
 
-	if pet == "yes"
-		puts "Great! Your spirit animal is proud!"
-	elsif pet == "no" 
-		puts "I'm sorry. :( Your spirit animal is disappointed."
-	else
-		puts "You didn't answer with 'yes' or 'no'"
-	end
+		
+	
