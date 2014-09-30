bc46319ab039f73996861198ad3008a57bf54eed
eliza-irene <elizabeth.korthof@gmail.com>
2014-09-30 11:39:03 -0400

    Apply OOP with Jerry's edits

--- talkative_robot.rb
+++ talkative_robot.rb
@@ -7,13 +7,13 @@ def greeting
 end
 
 class Person
-	def initialize(user)
-		@name 			= user[:name]
-		@age 			= user[:age]
-		@gender 		= user[:gender]
-		@color 			= user[:color]
-		@spirit_animal 	= user[:spirit_animal]
-		@pet 			= user[:pet]
+	def initialize(args)
+		@name 			= args[:name]
+		@age 			= args[:age]
+		@gender 		= args[:gender]
+		@color 			= args[:color]
+		@spirit_animal 	= args[:spirit_animal]
+		@pet 			= args[:pet]
 	end
 
 	def self.get_info_from_prompts
@@ -51,44 +51,28 @@ class Person
 		@age
 	end
 
-	def user_age
-		self.age
-	end
-
 	def gender
 		@gender
-	end
-
-	def user_gender
-		self.gender_greeting
 	end
 
 	def color
 		@color
 	end
 
-	def user_color
-		self.color
-	end
-
 	def spirit_animal
 		@spirit_animal
-	end
-
-	def user_spirit_animal
-		self.spirit_animal
 	end
 
 	def pet
 		@pet
 	end
 
-	def user_pet
-		self.pet
+	def female?
+		@gender == "female"
 	end
 
 	def age_gender_greeting
-		gender_greeting = @gender == "female" ? "Ms." : "Mr."
+		gender_greeting = female? ? "Ms." : "Mr."
 		age_message = @age >= 21 ? "You're old enough to drink! Yes!" : "You're not old enough to drink."
 		puts "Hi #{gender_greeting} #{@name} who is #{@age} years old. #{age_message}"
 	end
@@ -96,41 +80,47 @@ class Person
 	def until_75_statement
 	case
 		when @age < 75
-		young_future_age = 75 - @age
-		puts "You will be 75 years old in #{young_future_age} years!"
+			young_future_age = 75 - @age
+			puts "You will be 75 years old in #{young_future_age} years!"
 		when @age > 75
-		old_future_age = @age - 75
-		puts "#{old_future_age} years ago you turned 75! Wow, you're up there!"
+			old_future_age = @age - 75
+			puts "#{old_future_age} years ago you turned 75! Wow, you're up there!"
 		when @age == 75
-		puts "You're exactly 75 years old!"
+			puts "You're exactly 75 years old!"
 		end
 	end
 
-	def cool_with_first_initial
+	def first_initial
 		first_initial = @name.chars.first
+	end
+
+	def ask_about_first_initial
 		puts "Do you mind if I call you #{first_initial}? (yes or no)"
-		initial_answer = gets.chomp.downcase
+		@first_initial_answer = gets.chomp.downcase
+	end
+
+	def cool_with_first_initial?
+		@first_initial_answer == "yes"
 	end
 
 	def first_initial_preference
-		if cool_with_first_initial == "no" 
-			@name = @name.chars.first 
-			puts "Okay, I'll call you #{@name}."	
-		else
-			puts "Don't worry about it, I'll call you #{@name}."
+		ask_about_first_initial
+		puts cool_with_first_initial? ? "Okay, I'll call you #{@name}." : "Don't worry about it, I'll call you #{@name}."
+		if cool_with_first_initial? 
+			@name = @name.first_initial
 		end
 	end
 
 	def whats_up_message
-		@name.upcase!
-		puts "Hey #{@name}, where are you going!?"
+		name_caps = @name.upcase
+		puts "Hey #{name_caps}, where are you going!?"
 		puts "Yo 'Dude', are you having a good day? (yes or no)"
 		whats_up_answer = gets.chomp
 		puts whats_up_answer == "yes" ? "That's good news." : "Sorry to hear that."
 	end
 
 	def spirit_animal_message
-		puts "#{@name}, you are now channeling the mighty #{@spirit_animal} whose power color is #{@color}!  You are almost ready to take over the world!!!"
+		puts "Whoa! #{@name}, you are now channeling the mighty #{@spirit_animal} whose power color is #{@color}!  You are almost ready to take over the world!!!"
 	end
 
 	def has_pet?
@@ -144,13 +134,13 @@ class Person
 end
 
 class User < Person
-	def initialize(user)
+	def initialize(args)
 		super
 	end
 end
 
 class Author < Person
-	def initialize(user)
+	def initialize(args)
 		super
 	end
 
@@ -162,22 +152,27 @@ end
 
 class Grocery
 	def initialize(grocery_list)
-		@grocery_list = grocery_list
+		@items = grocery_list
 	end
 
-	def grocery_list
-		@grocery_list
+	def items
+		@items
 	end
 
-	def self.get_grocery_list
-		@grocery_list = IO.read("grocery_list.txt").chomp.split(", ")
-		@grocery_list.map! { |item| item.downcase }
+	def self.get_list
+		@items = IO.read("grocery_list.txt").chomp.split(", ")
+		@items.map! { |item| item.downcase }
 	end
 
-	def grocery_items_needed
+	def items_needed
 		puts "Time to go to the grocery store. Items to buy:"
-		@grocery_list.each_index do |i| puts "Item #{ i +1 } -- #{@grocery_list[i]}"
+		@items.each_index do |i| 
+			puts "Item i +1 -- #{@items[i]}"
 		end
+	end
+
+	def random_item
+		@random_item ||= @items.sample
 	end
 
 	def grab_random_item
@@ -188,32 +183,32 @@ class Grocery
 	def random_item_response
  		if @answer == "yes"  
  			puts "Great." 
-     		@grocery_list.delete(random_item)
+     		@items.delete(random_item)
  		else 
  			puts "Oh no!"
  		end
- 		@grocery_list
+ 		@items
 	end
 
 	def butter_reminder
  		puts "Oh yeah, don't forget the butter!"
- 		@grocery_list << "butter"
+ 		@items << "butter"
 	end
 
 	def create_new_txt(txt_file_name)
-		IO.write("new_grocery_list.txt", @grocery_list)
+		IO.write("new_grocery_list.txt", @items)
 	end
 
 	def create_csv(csv_file_name)
 		CSV.open(csv_file_name, "w" ) do |csv|
   			csv << ["Item Number", "Item Name"]
-  			@grocery_list.each_index do |i| 
-  				csv << ["#{i+1}", @grocery_list[i]]
+  			@items.each_index do |i| 
+  				csv << ["#{i+1}", @items[i]]
   			end
   		end
 	end
 
-	def save_grocery_list(txt_file_name, csv_file_name)
+	def save_list(txt_file_name, csv_file_name)
 		create_new_txt(txt_file_name)
 		create_csv(csv_file_name)
 	end
@@ -221,24 +216,23 @@ class Grocery
 	def read_from_csv
 		col_data0 = []
 		col_data1 = []
-		CSV.foreach('groceries_including_butter.csv') {|row| col_data0 << row[0] + " " + row[1]}
-		puts "This is the current grocery list:"
+		CSV.foreach('groceries_including_butter.csv') {|row| 
+			col_data0 << row[0] + " " + row[1]}
+		puts "Okay, here's the updated list:"
 		col_data0.shift
+		col_data0
 		puts col_data0
 	end
 
-	def grocery_stuff(txt_file_name, csv_file_name)
-		grocery_items_needed
+	def go_shopping(txt_file_name, csv_file_name)
+		items_needed
 		response = grab_random_item
 		random_item_response
 		butter_reminder
-		save_grocery_list(txt_file_name, csv_file_name)
+		save_list(txt_file_name, csv_file_name)
 		read_from_csv
 	end
 
-	def random_item
-		@random_item ||= @grocery_list.sample
-	end
 end
 
 def select_by_name(person, first_name)
@@ -259,9 +253,9 @@ greeting
 @user.spirit_animal_message
 @user.pet_statement
 
-items = Grocery.get_grocery_list
-@grocery_list = Grocery.new(items)
-@grocery_list.grocery_stuff("new_grocery_list.txt", "groceries_including_butter.csv")
+items = Grocery.get_list
+@items = Grocery.new(items)
+@items.go_shopping("new_grocery_list.txt", "groceries_including_butter.csv")
 
 author_info = Author.author_info
 @author = Author.new(author_info)
